127b48a9e7e77ece8554a677b45629134ba86b3c
maxim <makfedorovtooo@gmail.com>
2023-04-12 14:25:09 +0300

    all done

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,7 +9,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Set<Employee> emp = new HashSet<>();
         Employee emp1 = new Employee("Ivan", "Ivanov", 45, "Manager");
         Employee emp2 = new Employee("Ivan", "Ivanov", 50, 1000.0, "Manager");
         Employee emp3 = new Employee("Ivan", "Ivanov", 50, 1000.0, "Manager");
